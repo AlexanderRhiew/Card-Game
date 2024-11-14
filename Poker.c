34d10b1084583939d789242a5396a9e3a254c41f
AlexanderRhiew <alexanderrhiew@gmail.com>
2024-11-14 13:21:27 -0700

    Updated
    Click here

--- Poker.c
+++ Poker.c
@@ -1,29 +1,54 @@
+//Alex Rhiew + Taysom StolWorthy
+//12/6/2024
+//Card_Game
+//Poker Variation, decks. 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/*
+
 typedef struct card_S {
     char suit;
     int rank;
 
  } card;
- 36 element array of card element
+/* 36 element array of card element
 array of Hearts, Diamonds, Spades, and Clubs
 Count = 0
 loop to assign the suit (4 iterations)
-    loop to assign the rank ( 9 iterations) 
+    loop to assign the rank (9 iterations) 
     array[count].suit
     array[count].rank
 
 
-void card_face {}
-*/
+void card_suit {
 
+}
+
+void card_rank {
+
+}
+
+void printfct {
+
+}
+
+void shuffle_deck {
+
+}
+
+
+*/
 int main(void) {
     int test, i, c_count, coins, faces;
-    scanf("%d", &test);
-    printf("Hello World! %d", test);
+    printf("Enter an amount of coins to start playing: ");
+    scanf("%d", &coins);
+    printf("You have been dealt 5 cards.")
+
+    int card[36]
+
+
     
     return 0;
 }
