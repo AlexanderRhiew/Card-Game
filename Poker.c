7fd8e65837f72ee82cbc10cd9954f24327e30592
TaysomS <taystolworthy@gmail.com>
2024-11-14 13:43:52 -0700

    Update Poker.c

--- Poker.c
+++ Poker.c
@@ -23,9 +23,21 @@ loop to assign the suit (4 iterations)
     array[count].rank
 
 
-void card_suit {
-
-}
+void card_suit(card list[], int num){
+  int i = 1; 
+  int track = 0; 
+      while( track < num){
+          if(i = 10){
+              i = 0; 
+          }
+    
+    list[i].suit = i; 
+          i++; 
+          track++;
+          printf("%d\n", list[i].suit); 
+      }
+      
+  }
 
 void card_rank {
 
@@ -44,6 +56,7 @@ void shuffle_deck {
 int main(void) {
     int test, i, c_count, coins_entered, faces;
     int coins = 100;
+    int num = 36; 
     printf("Enter an amount of coins to start playing: ");
     scanf("%d", &coins_entered);
     if ((coins_entered <= 0) || (coins_entered > 100)) {
