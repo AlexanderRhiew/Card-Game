002785c38755410d283e9c3ae6a1a27d942fb17e
AlexanderRhiew <alexanderrhiew@gmail.com>
2024-11-14 12:51:14 -0700

    Edit P2

---
+++ Poker.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+
+typedef struct card_S {
+    char suit;
+    int rank;
+
+ } card;
+/* 36 element array of card element
+array of Hearts, Diamonds, Spades, and Clubs
+Count = 0
+loop to assign the suit (4 iterations)
+    loop to assign the rank ( 9 iterations) 
+    array[count].suit
+    array[count].rank
+*/
+
+void card_face {}
+
+
+int main(void) {
+    int test, i, c_count, coins, faces;
+    scanf("%d", &test);
+    printf("Hello World! %d", test);
+
+    int card[36]
+
+
+    
+    return 0;
+}
